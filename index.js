40ac228da21a9b70cfe7c3d512a9c5f8c1d41675
Michael Hart <michael.hart.au@gmail.com>
2019-07-26 14:31:39 -0400

    Fix error message property

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ addEventListener('fetch', event => {
       return res
     } catch (e) {
       event.waitUntil(log(e, event.request))
-      return new Response(e.msg || 'An error occurred!', { status: e.statusCode || 500 })
+      return new Response(e.message || 'An error occurred!', { status: e.statusCode || 500 })
     }
   })
 })
